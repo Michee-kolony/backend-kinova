10f0079c117c88aeae29e629575d17bc746aa657
Michée Kolony <micheekolony71@gmail.com>
2026-07-15 16:42:48 +0100

    ajout affichage photo notif

--- services/notification.js
+++ services/notification.js
@@ -1,30 +1,40 @@
 const admin = require("../config/firebase");
 
-
 exports.sendNotification = async (token, article) => {
 
+    const image =
+        article.images && article.images.length > 0
+            ? article.images[0]
+            : "";
 
     const message = {
 
-        token: token,
+        token,
 
         notification: {
-
             title: article.nom,
-
             body: article.description
+        },
 
+        android: {
+            notification: {
+                imageUrl: image
+            }
+        },
+
+        apns: {
+            fcmOptions: {
+                image: image
+            }
         },
 
         data: {
-
-            articleId: article._id.toString()
-
+            articleId: article._id.toString(),
+            nom: article.nom,
+            image: image
         }
 
     };
 
-
     return admin.messaging.send(message);
-
 };
