020556e38894d414723701de6f4d55c2514a7cc1
Michée Kolony <micheekolony71@gmail.com>
2026-09-01 17:35:15 +0100

    ajout socket io

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 require("dotenv").config();
 const http = require('http');
 const express = require('express');
+const { Server } = require('socket.io');
 const app = require('./app');
 
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`Client connecté : ${socket.id}`);
+
+  socket.on('join', (userId) => {
+    socket.join(userId);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Client déconnecté : ${socket.id}`);
+  });
+});
+
+app.set('io', io);
 
 const PORT = process.env.PORT || 3000;
 
